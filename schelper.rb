11a41e4c408e6cdcede1617e5d40ae1d1d805f37
mdsauce <max.dobeck@saucelabs.com>
2019-07-08 15:09:47 -0700

    Manually updating all ver. numbers now.

--- schelper.rb
+++ schelper.rb
@@ -1,14 +1,13 @@
-ver = "1.0.4"
 class Schelper < Formula
   desc "Analyze log files from Sauce Connect"
   homepage "https://github.com/mdsauce/schelper"
   url "https://github.com/mdsauce/schelper/archive/v1.0.4.tar.gz"
   sha256 "8db455d07ec47da53aa4e791fe6b9de7695cf63330bcc9d120d0d91d7f5a063d"
   depends_on "go" => :build
-  version ver
+  version 1.0.4
 
   def install
-    system "go", "build", "-ldflags, "-X github.com/mdsauce/schelper/cmd.CurVersion="+ver, " "-o", bin/"schelper", "."
+    system "go", "build", "-ldflags, "-X github.com/mdsauce/schelper/cmd.CurVersion=1.0.4", " "-o", bin/"schelper", "."
   end
 
   test do
