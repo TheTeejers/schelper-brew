2874eb14d95c80c5154d7c5bea0968d02f2808e1
mdsauce <max.dobeck@saucelabs.com>
2019-07-08 15:10:40 -0700

    Forgot double-quotes

--- schelper.rb
+++ schelper.rb
@@ -4,7 +4,7 @@ class Schelper < Formula
   url "https://github.com/mdsauce/schelper/archive/v1.0.4.tar.gz"
   sha256 "8db455d07ec47da53aa4e791fe6b9de7695cf63330bcc9d120d0d91d7f5a063d"
   depends_on "go" => :build
-  version 1.0.4
+  version "1.0.4"
 
   def install
     system "go", "build", "-ldflags, "-X github.com/mdsauce/schelper/cmd.CurVersion=1.0.4", " "-o", bin/"schelper", "."
