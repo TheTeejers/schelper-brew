4df0977c4226b92dc6c4aff4d950062ca8bac42f
mdsauce <max.dobeck@saucelabs.com>
2019-08-02 16:52:45 -0700

    Updating to latest version 1.1.0

--- schelper.rb
+++ schelper.rb
@@ -1,13 +1,13 @@
 class Schelper < Formula
   desc "Analyze log files from Sauce Connect"
   homepage "https://github.com/mdsauce/schelper"
-  url "https://github.com/mdsauce/schelper/archive/v1.0.5.tar.gz"
-  sha256 "dc218b88bddbc64b57387ac3369a0831a37f09cb795192db3e79ba01344d8e1e"
+  url "https://github.com/mdsauce/schelper/archive/v1.1.0.tar.gz"
+  sha256 "518a200a4b3552c2702e68f8b6583dd74ee7fc6f3aeb74421956d2b94447801d"
   depends_on "go" => :build
-  version "1.0.5"
+  version "1.1.0"
 
   def install
-    system "go", "build", "-ldflags", "-X github.com/mdsauce/schelper/cmd.CurVersion=1.0.5", "-o", bin/"schelper", "."
+    system "go", "build", "-ldflags", "-X github.com/mdsauce/schelper/cmd.CurVersion=1.1.0", "-o", bin/"schelper", "."
   end
 
   test do
