3e087862c01f196e9f940ade7055bb142bc95d1c
mdsauce <max.dobeck@saucelabs.com>
2019-08-02 15:56:25 -0700

    Updating to 1.0.5.  New UI and better error detection.

--- schelper.rb
+++ schelper.rb
@@ -1,13 +1,13 @@
 class Schelper < Formula
   desc "Analyze log files from Sauce Connect"
   homepage "https://github.com/mdsauce/schelper"
-  url "https://github.com/mdsauce/schelper/archive/v1.0.4.tar.gz"
+  url "https://github.com/mdsauce/schelper/archive/v1.0.5.tar.gz"
   sha256 "e6d0c90f10ff2c41702a287654111b48ead6710633957ee65b0fd9cb801cbc3e"
   depends_on "go" => :build
-  version "1.0.4"
+  version "1.0.5"
 
   def install
-    system "go", "build", "-ldflags", "-X github.com/mdsauce/schelper/cmd.CurVersion=1.0.4", "-o", bin/"schelper", "."
+    system "go", "build", "-ldflags", "-X github.com/mdsauce/schelper/cmd.CurVersion=1.0.5", "-o", bin/"schelper", "."
   end
 
   test do
