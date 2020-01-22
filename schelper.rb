e7446cd75ab3d65b986abc7711422c9f12e6621a
mdsauce <max.dobeck@saucelabs.com>
2020-01-22 15:41:56 -0800

    Adding sha

--- schelper.rb
+++ schelper.rb
@@ -2,7 +2,7 @@ class Schelper < Formula
   desc "Analyze log files from Sauce Connect"
   homepage "https://github.com/mdsauce/schelper"
   url "https://github.com/mdsauce/schelper/archive/v1.1.3.tar.gz"
-  sha256 ""
+  sha256 "288c271417569298f7952ce5f6e4c19b73e129b9bcda2f0a3f5e20b85f2c3736"
   depends_on "go" => :build
   version "1.1.3"
 
